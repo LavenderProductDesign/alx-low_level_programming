7df8d268000ac34f74320fd46a6ebffb77c2cd6f
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-11 17:20:23 +0300

    Numbers to the base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -print all single digit numbers of base 10 starting from 0
+ *
+ * Description: using the main function
+ * this program prints all aingle digit numbers of base 10 from 0
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+	for (i = 0; i < 10; i++)
+	printf("%i", i);
+	printf("\n");
+
+return (0);
+}
