0dad807ba2534262159a43c79d6977a36376ba87
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-11 17:34:37 +0300

    Putchar print base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -print all single digit numbers of base 10 starting from 0
+ *
+ * Description: using the main function
+ * this program prints all aingle digit numbers of base 10 from 0
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+	for (c = 0; c < 10; c++)
+	putchar(c);
+	putchar('\n');
+
+return (0);
+}
