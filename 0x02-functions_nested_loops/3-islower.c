364f2cd3dca8d2de9a858da993a25a4a6ed5cd14
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-15 17:41:16 +0300

    prints sign of a number

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * * _islower - check if the character is lowerCase
- * * @c: the character to be cheked
+ * * @c: the character to be checked
  * * Return: 1 if lower, 0 if capital
  */
 
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - function declaration
+ * @n: integer to be tested
+ * Return: 1 if positive, 0 if 0, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+												}
+}
