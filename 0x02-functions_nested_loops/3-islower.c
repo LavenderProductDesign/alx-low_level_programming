aac5e532e981ed14ae9625a2ace0b6e168ecc770
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-15 16:45:58 +0300

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * * _islower - check if the character is lowerCase
+ * * @c: the character to be cheked
+ * * Return: 1 if lower, 0 if capital
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
