b7d79201413ee38f85d0872517b058a35bd037ca
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-15 17:48:07 +0300

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_abs - entry to the program
+ * @n: argument to be checked
+ * Return: the absolte value of @n
+ */
+
+int _abs(int n)
+{
+		return (n < 0 ? -1 * n : n);
+}
