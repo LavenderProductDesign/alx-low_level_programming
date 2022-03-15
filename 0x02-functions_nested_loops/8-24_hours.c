421febe91f88caa5c9a79c28992efc0736ede882
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-15 19:35:15 +0300

    Hours of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ ** jack_bauer - entry point
+ ** Return: void
+ */
+
+void jack_bauer(void)
+{
+int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+	for (j = 0; j < 60; j++)
+	{
+		_putchar(i / 10 + 48);
+		_putchar(i % 10 + 48);
+		_putchar(':');
+		_putchar(j / 10 + 48);
+																							_putchar(j % 10 + 48);
+																							_putchar('\n');
+	}
+	}
+}
