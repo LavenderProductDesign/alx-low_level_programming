791f7b419bdc8e94947d66c90b77375d83234415
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-15 14:35:38 +0300

    Main files

---
+++ 0x02-fuctions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - print the word "_putchar"
+ * Description: using the main fuction
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+	_putchar(p[i]);
+	}
+	_putchar('\n');
+return (0);
+}
