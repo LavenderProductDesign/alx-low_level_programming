c335a994e6c9be75fa6191140cb1d5a02dddabcf
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-15 16:14:06 +0300

    prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ ** print_alphabet_x10 - entry point for the program
+ * Description: Print lowercase alphabet 10 times
+ * Return: 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+
+		for (ch = 'a'; ch <= 'z' ; ch++)
+		{
+			-putchar(ch);
+		}
+		_putchar(\n');
+	}
+}
