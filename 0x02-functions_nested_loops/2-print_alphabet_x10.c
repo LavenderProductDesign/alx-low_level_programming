521958b1cb5bba947fa7b3e0c839dee2d1e3d4e4
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-15 16:18:31 +0300

    Print aplhabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 
 		for (ch = 'a'; ch <= 'z' ; ch++)
 		{
-			-putchar(ch);
+			_putchar(ch);
 		}
-		_putchar(\n');
+		_putchar('\n');
 	}
 }
