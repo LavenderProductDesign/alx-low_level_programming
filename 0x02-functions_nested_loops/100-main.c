be7b937991c7f0674c66dbb8e5448865fbe32fe5
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-16 23:51:57 +0300

    100 times table

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,10 +1,13 @@
-n - check the code.
- *
- *  * Return: Always 0.
- *   */
- int main(void)
+#include "main.h"
+
+/**
+ *  * main - check the code.
+ *   *
+ *    * Return: Always 0.
+ */
+int main(void)
 {
-	    print_times_table(3);
+	   print_times_table(3);
 	        _putchar('\n');
 		    print_times_table(5);
 		        _putchar('\n');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int);
+void print_times_table(int);
 #endif /* main_h */
