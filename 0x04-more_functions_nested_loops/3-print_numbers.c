c9c41b0620869a356cadd4855a801180c468e564
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-18 00:37:59 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ ** print_numbers - print numbers (0 to 9)
+ **
+ ** Return: 0 (success)
+ */
+
+void print_numbers(void)
+{
+
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+
+	_putchar('\n');
+}
