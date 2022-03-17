5e7abcc95f2077c6ce64009ae457eb4c2495b23d
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-18 00:32:36 +0300

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * * mul - multiplies two integers a and b)
+ * * @a: first integer
+ * * @b: second integer
+ * * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
