abbb7471a1c623e90b1871225a80c0c3c7918eed
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-30 16:30:16 +0300

    prints a string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_FILE
+#define MAIN_H_FILE
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int helper(int n, int i);
+int is_prime_number(int n);
+int helper_prime(int n, int i, int limit);
+
+#endif
