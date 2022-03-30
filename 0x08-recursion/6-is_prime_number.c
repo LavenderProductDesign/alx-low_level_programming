230013090435c2078505f9b59e9e5c408c911d27
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-03-30 16:59:05 +0300

    returns 1 if the input integer is a prime number otherwise 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * is_prime_number - func that returns 1 if a num is a prime num, 0 otherwise
+ * @n: number for input
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	return (helper_prime(n, 2, n / 2));
+}
+
+/**
+ *  helper_prime - blank
+ * @n: checkl
+ * @i: for the increment
+ * @limit: stop
+ * Return: 0
+ */
+
+int helper_prime(int n, int i, int limit)
+{
+	if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
+		return (0);
+	else if (n % i != 0 && i <= limit)
+		return (helper_prime(n, i + 1, limit));
+	else
+		return (1);
+}
