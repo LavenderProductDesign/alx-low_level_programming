a20f8a7de0da3df5fabbdc8b0150f731dea4effc
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-04-05 15:59:41 +0300

    concatenates

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: paramater for string 1
+ * @s2: parameter for string 2
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *strDup;
+int a, b;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+a = b = 0;
+while (s1[a] != '\0')
+a++;
+while (s2[b] != '\0')
+b++;
+strDup = malloc(sizeof(char) * (a + b + 1));
+if (strDup == NULL)
+return (NULL);
+a = b = 0;
+while (s1[a] != '\0')
+{
+strDup[a] = s1[a];
+a++;
+}
+while (s2[b] != '\0')
+{
+strDup[a] = s2[b];
+a++, b++;
+}
+strDup[a] = '\0';
+return (strDup);
+}
