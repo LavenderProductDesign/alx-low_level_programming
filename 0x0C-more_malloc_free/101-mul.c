1062a9ae8aa37e6cae76fe0d42b180054fada881
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-04-07 15:53:52 +0300

    multiplies edited

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,99 +1,101 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+int _strlen(char *s);
+int _toint(char *s);
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-int is_digit(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * main - multiplies two positive numbers
+ * main - multiplies two numbers
  * @argc: number of arguments
- * @argv: array of arguments
+ * @argv: array of pointers to string arguments
  *
- * Return: always 0 (Success)
+ * Return: always 0 for success
  */
+
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	long int num1, num2;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	if (argc != 3)
 	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+		printf("Error\n");
+		exit(98);
 	}
-	for (i = 0; i < len - 1; i++)
+	if (_toint(argv[1]) == 0)
 	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
+		printf("Error\n");
+		exit(98);
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
+	if (_toint(argv[2]) == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (*argv[1] == 48)
+		num1 = 0;
+	else
+		num1 = _toint(argv[1]);
+	if (*argv[2] == 48)
+		num2 = 0;
+	else
+		num2 = _toint(argv[2]);
+	printf("%ld\n", num1 * num2);
 	return (0);
+}
+
+/**
+ * _strlen - prints out the length of the specified string
+ * @s: string which length is to be calculated
+ *
+ * Return: the length of the string (int)
+ */
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (*s)
+	{
+		size++;
+		s++;
+	}
+	return (size);
+}
+
+/**
+ * _toint - convert str to int
+ * @s: string value
+ *
+ * Return: int value
+ */
+int _toint(char *s)
+{
+	int size, i, isNegative;
+	long int number;
+
+	size = _strlen(s);
+	number = 0;
+	isNegative = 0;
+	for (i = 0; i < size; i++)
+	{
+		if (s[i] == 45)
+		{
+			isNegative = 1;
+			continue;
+		}
+		else if (s[i] > 47 && s[i] < 58)
+		{
+			number = (number * 10) + (s[i] - 48);
+			/* printf("%dth - %d\n", i, number); */
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	/* printf("%s => %ld : len - %d\n", s, number, size); */
+	if (isNegative)
+		return (-1 * number);
+	return (number);
 }
