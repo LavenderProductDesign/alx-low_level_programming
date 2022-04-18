f43e9f20057fdb388491176cfa88a6c6ec47c5b4
LavenderProductDesign <lavender.productdesign@gmail.com>
2022-04-18 22:06:31 +0300

    printf files

---
+++ printf/main_test_files/test-hex.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	_printf("%x\n", 45);
+	_printf("%x\n", 4512);
+	_printf("%X\n", 45);
+	_printf("%X\n", 4512);
+	return (0);
+}
---
+++ printf/main_test_files/test-unsigned.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned int a;
+	int b;
+
+	a = -8;
+	b = _printf("%u\n", a);
+	_printf(">%i\n", b);
+	b = printf("%u\n", a);
+	printf(">%i\n", b);
+	return (0);
+}
